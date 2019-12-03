5e7bf81d7c7b0259e095f2ded76b93e40bbb05be
Tord Joranger <TORDJ@equinor.com>
2019-12-03 09:06:55 +0100

    added files

---
+++ src/QueueReceiver.Infrastructure/Data/PersonProjectConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QueueReceiver.Core.Models;
+
+namespace QueueReceiver.Infrastructure.Data
+{
+    public class PersonProjectConfiguration : IEntityTypeConfiguration<PersonProject>
+    {
+        public void Configure(EntityTypeBuilder<PersonProject> builder)
+        {
+            builder.HasKey(pp => new { pp.ProjectId, pp.PersonId });
+            builder.HasOne(pp => pp.Project)
+                .WithMany()
+                .HasForeignKey(pp => pp.ProjectId);
+
+            builder.Property(p => p.IsVoided)
+                .HasConversion(
+                    b => b ? 'Y' : 'N',
+                    c => c.Equals('Y'));
+        }
+    }
+}
