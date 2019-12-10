dd37ff907ef6067584cc4b4425091941b4295a91
Lykke Larsen <norl@statoil.com>
2019-12-10 12:04:41 +0100

    Add tests

--- tests/QueueReceiver.Infrastructure.UnitTests/Repositories/PersonRestrictionRoleRepositoryTests.cs
+++ tests/QueueReceiver.Infrastructure.UnitTests/Repositories/PersonRestrictionRoleRepositoryTests.cs
@@ -1,36 +1,53 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using QueueReceiver.Core.Interfaces;
+using Moq;
+using QueueReceiver.Core.Models;
+using QueueReceiver.Infrastructure.Data;
+using QueueReceiver.Infrastructure.Repositories;
 
 namespace QueueReceiver.UnitTests.Infrastructure.Repositories
 {
     [TestClass]
     public class PersonRestrictionRoleRepositoryTests
     {
-        public PersonRestrictionRoleRepositoryTests(
-            IPersonRestrictionRoleRepository personRestrictionRoleRepository)
-        {
-            _personRestrictionRoleRepository = personRestrictionRoleRepository;
-        }
+        private const long personId = 2;
+        private const string restrictionRole = "NO_RESTRICTION";
+        private const string plantId = "Dagobath";
 
-        private IPersonRestrictionRoleRepository _personRestrictionRoleRepository;
 
         [TestMethod]
-        public async Task AddIfNotExistTest()
+        public async Task AddSync_DoesNothing_IfRoleAlreadyExists()
         {
-            // Testing AddIfNotExistAsync(string plantId, string restrictionRole, long personId)
-            
-            
             //Arrange
-            const string plantId = "PCS$HEIMDAL";
-            const string restrictionRole = "NO_RESTRICTION";
-            const long personId = 1;
+            var mockContext = new Mock<ApplicationDbContext>();
+            mockContext.Setup(cxt => cxt.PersonRestrictionRoles.Find(restrictionRole, personId, plantId))
+                .Returns(new PersonRestrictionRole(plantId, restrictionRole, personId)); ;
+            mockContext.Setup(cxt => cxt.PersonRestrictionRoles.AddAsync(It.IsAny<PersonRestrictionRole>(), default))
+                .Returns(Task.FromResult(new EntityEntry<PersonRestrictionRole>(new MockInternal())));
+
+            var repository = new PersonRestrictionRoleRepository(mockContext.Object);
 
             //Act
-            var x = _personRestrictionRoleRepository.AddIfNotExistAsync(plantId, restrictionRole, personId);
+            await repository.AddIfNotExistAsync(plantId, restrictionRole, personId);
 
             //Assert
-            
+            mockContext.Verify(cxt => cxt.PersonRestrictionRoles.Find(restrictionRole, personId, plantId), Times.Once);
+            mockContext.Verify(cxt => cxt.PersonRestrictionRoles.AddAsync(It.IsAny<PersonRestrictionRole>(), default), Times.Never);
+        }
+
+        private class MockInternal : InternalEntityEntry
+        {
+            private readonly PersonRestrictionRole personRestrictionRole = new PersonRestrictionRole(plantId, restrictionRole, personId);
+
+            public MockInternal()
+                : base(default, new EntityType("mock", new Model(), default))
+            {
+            }
+
+            public override object Entity => personRestrictionRole;
         }
     }
 }
