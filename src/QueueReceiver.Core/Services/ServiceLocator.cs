fd18e8f0b005acbbafd47ae8df9170bbb7a7017b
Henning Bjørgo <HHBJ@equinor.com>
2020-01-23 08:11:22 +0100

    Comments.

--- src/QueueReceiver.Core/Services/ServiceLocator.cs
+++ src/QueueReceiver.Core/Services/ServiceLocator.cs
@@ -7,7 +7,7 @@ namespace QueueReceiver.Core.Services
     /// A wrapper around ServiceScopeFactory to make it easier to fake out with MOQ.
     /// </summary>
     /// <see cref="https://stackoverflow.com/a/53509491/54288"/>
-    public sealed class ServiceLocator : IServiceLocator
+    public sealed class ServiceLocator : IServiceLocator //TODO: No need for this class - remove.
     {
         private readonly IServiceScopeFactory _factory;
         private IServiceScope? _scope;
