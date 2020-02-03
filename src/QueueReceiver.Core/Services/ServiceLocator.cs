4616e0aa74d3d3b4e3b0dcf21881ed16b26cfcce
Tord Joranger <TORDJ@equinor.com>
2020-02-03 10:55:01 +0100

    easier to mock

---
+++ src/QueueReceiver.Core/Services/ServiceLocator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QueueReceiver.Core.Interfaces;
+
+
+namespace QueueReceiver.Core.Services
+{
+    public sealed class ServiceLocator : IServiceLocator
+    {
+        private readonly IServiceScopeFactory _factory;
+        private IServiceScope? _scope;
+
+        public ServiceLocator(IServiceScopeFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public T GetService<T>()
+        {
+            CreateScope();
+
+            return _scope!.ServiceProvider.GetService<T>();
+        }
+
+        public IServiceScope CreateScope()
+        {
+            return _factory.CreateScope();
+        }
+
+        public void Dispose()
+        {
+            _scope?.Dispose();
+            _scope = null;
+        }
+    }
+}
